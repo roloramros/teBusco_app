fbe8bc39c686c029e7e711f38c6dd00e543d9098
roloramros <roloramros@gmail.com>
2026-05-13 11:04:40 -0400

    feat(model): add verificado field to User model

--- app/src/main/java/com/codram/terecojo/data/model/AuthResponse.java
+++ app/src/main/java/com/codram/terecojo/data/model/AuthResponse.java
@@ -24,11 +24,14 @@ public class AuthResponse {
         @SerializedName("provincia_id")
         private Integer provinciaId;
 
+        private boolean verificado;
+
         public String getId() { return id; }
-        public String getNombre() { return nombre; }
+        public String getNombre() { return nombre; }    
         public String getUsername() { return username; }
         public String getTipo() { return tipo; }
         public Integer getMunicipio_id() { return municipioId; }
         public Integer getProvincia_id() { return provinciaId; }
+        public boolean isVerificado() { return verificado; }
     }
 }
